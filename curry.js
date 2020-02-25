7835abc9f2b339256bda29c4f2f6ee283f4c0549
Illia Chaban <illia.chaban.8@gmail.com>
2020-02-24 22:01:32 -0500

    refactored

--- curry.js
+++ curry.js
@@ -1,14 +1,14 @@
 const add = (a,b,c,d) => a + b + c + d;
 
 const curry = (fn, ...args) => {
-  if (args.length >= fn.length) {
-    return fn(...args);
+  if (args.length < fn.length) {
+    return (...more) => curry(fn, ...args, ...more);
   }
-  return (...args2) => {
-    const curried = fn.bind(null, ...args);
-    return curry(curried, ...args2);
-  };
+  // invoke the function if all expected parameters were specified
+  return fn(...args);
 }
 
 const fpAdd = curry(add)
 // fpAdd(1,2)(3)(4)  --> 10
+// fpAdd(1,2,3)(4)  --> 10
+// fpAdd(1)(2)(3)(4)  --> 10
